8197975215d7f36ff4b0c54acdc0ee41e7cfdeaf
wkolowski <wkolowski@protonmail.com>
2022-10-03 23:07:51 +0200

    Defined the free category.

--- Instances/Graph.v
+++ Instances/Graph.v
@@ -109,21 +109,54 @@ Instance GraphCat : Cat :=
   id := GraphId;
 }.
 Proof.
-  - intros A B C [f1 f1' Hf1 Hf1'] [f2 f2' Hf2 Hf2'] [Hf Hf']
-                 [g1 g1' Hg1 Hg1'] [g2 g2' Hg2 Hg2'] [Hg Hg']
-    ; split; cbn in *; intros.
-    + now rewrite Hg, Hf.
-    + now rewrite Hg', Hf'.
-  - now graph.
-  - now graph.
-  - now graph.
+  - intros A B C f f' [Hf1 Hf2] g g' [Hg1 Hg2]; cbn in *.
+    split; intros.
+    + now rewrite Hf1, Hg1.
+    + now rewrite Hf2, Hg2.
+  - now cbn.
+  - now cbn.
+  - now cbn.
 Defined.
 
-(* These are only finite paths *)
-Inductive path {G : Graph} : vertices G -> vertices G -> Type :=
-| idpath : forall v : vertices G, path v v
-| edgepath :
-  forall (v1 v2 v3 : vertices G) (e : edges G),
-    path v1 v2 -> src e = v2 -> tgt e = v3 -> path v1 v3.
+(** Now we define the free category for the given graph G. *)
 
-(* TODO: define free category *)
+(** Morphisms are finite walks. *)
+Inductive Walk {G : Graph} : vertices G -> vertices G -> Type :=
+| Stop : forall v : vertices G, Walk v v
+| Step :
+  forall {v1 v2 v3 : vertices G} {e : edges G},
+    src e = v1 -> tgt e = v2 -> Walk v2 v3 -> Walk v1 v3.
+
+Arguments Stop {G v}.
+Arguments Step {G v1 v2 v3 e} _ _ _.
+
+Fixpoint wapp
+  {G : Graph} {v1 v2 v3 : vertices G}
+  (w1 : Walk v1 v2) : Walk v2 v3 -> Walk v1 v3 :=
+match w1 with
+| Stop => fun w2 => w2
+| Step p1 p2 w1' => fun w2 => Step p1 p2 (wapp w1' w2)
+end.
+
+#[refine]
+#[export]
+Instance FreeCat (G : Graph) : Cat :=
+{
+  Ob := vertices G;
+  Hom := Walk;
+  id := @Stop G;
+  comp := @wapp G;
+}.
+Proof.
+  - intros v1 v2.
+    split with (fun w1 w2 => w1 = w2).
+    now split; red; [| | apply transitivity].
+  - now intros v1 v2 v3 w12 w12' <- w23 w23' <-; cbn.
+  - induction f as [| v1 v2 v3 e p1 p2 f']; cbn; intros.
+    + easy.
+    + now rewrite IHf'.
+  - now cbn.
+  - induction f as [| v1 v2 v3 e p1 p2 f']; cbn.
+    + easy.
+    + now rewrite IHf'.
+Defined.
